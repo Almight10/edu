defca9bb7b882518307465a8aa20a6bd9c7d44a9
Frederick Ananda <derickerick48@gmail.com>
2025-12-24 17:56:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8432650142-13775",
+  "appId": "1:974017494896:web:b32da7552ef07f0f9fd9b5",
+  "apiKey": "AIzaSyDS0aKQfqBzUN53pavH1ESPu5zcuQ2BHPY",
+  "authDomain": "studio-8432650142-13775.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "974017494896"
+};
