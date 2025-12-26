d87df2020abe15ced92014f67050050201302634
Frederick Ananda <derickerick48@gmail.com>
2025-12-26 04:49:04 +0000

    tpi di netlify pakai akun lain

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,14 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
-// PENTING: Ganti dengan konfigurasi Firebase Anda yang sebenarnya
+// Your web app's Firebase configuration is now read from environment variables
 export const firebaseConfig = {
-  projectId: 'studio-8432650142-13775',
-  appId: '1:443833292497:web:9c6c196a63505c8227b958',
-  apiKey: "AIzaSyDSlqO7F6mF-ifwyo5i2p9A-V24zZNv5B4",
-  authDomain: 'studio-8432650142-13775.firebaseapp.com',
-  messagingSenderId: '443833292497',
-  storageBucket: 'studio-8432650142-13775.appspot.com',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
